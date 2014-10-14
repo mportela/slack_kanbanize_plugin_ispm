c778516a2349270e59800631a8fce1a7b0ecabb2
Marcio <marcio.cruz@ispm.com.br>
2014-10-14 11:25:03 -0300

    testes para casos padrão e primeiras customizações

--- slack_kanbanize_formatter_plugin/ispm_formatter.py
+++ slack_kanbanize_formatter_plugin/ispm_formatter.py
@@ -2,14 +2,15 @@
 
 def formatter(activity_data):
     """
-        this is a special formatter function to be used as a plugin of
-        slak-kambanize app
+        This is a special formatter function to be used as a plugin of
+        slak-kanbanize app
         Process the activitie object and Return the formatted_message for
         this activity
         Arguments:
         @activity_data - dict object with this format:
-                        {u'event': u'Task updated',
-                         u'text': u'New tag:', u'author': u'mportela'}
+            {u'event': u'Task updated',
+            u'text': u'New tag:',
+            u'author': u'mportela'}
         Return the formatted_string for the activity, based in the 'event'
     """
     events_emoji_traslator = {
@@ -28,16 +29,23 @@ def formatter(activity_data):
     event = activity_data.get(u'event', u'')
     user = u'*%s*' % activity_data.get(u'author', u'')
     text = activity_data.get(u'text', u'')
+
     try:
         emoji = u'%s ' % events_emoji_traslator[event]
-        #special ispm formats to override here
+
         if event == u'Assignee changed' and text == u'New assignee: None':
-            emoji = u':runner: '
+            msg = u':runner: %s deixou o card.' % user
+            return msg
+
         if event == u'Task moved' and\
-                         u"to 'In Progress.Revis\xe3o Interna'" in text:
-            emoji = u':mag_right: '
+            u"to 'In Progress.Revis\xe3o Interna'" in text:
+            msg = u':mag_right: %s enviou a tarefa para review.'
+
+            return msg % user
+
         if event == u'Tags changed' and u'New tag: empacotado' in text:
             emoji = u':package: '
+
     except KeyError, e:
         # case of event not know, just return the name with italic format
         event = u'_%s_' % event
---
+++ slack_kanbanize_formatter_plugin/tests.py
@@ -0,0 +1,59 @@
+# encoding: utf-8
+
+import unittest
+from ispm_formatter import formatter
+
+
+class IspmFormatterTests(unittest.TestCase):
+    def test_default_cases(self):
+        activity = {
+            u'event': u'Comment added',
+            u'text': u'teste',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u':speech_balloon: Usuário: *mportela* Evento: Comment added: teste')
+
+    def test_assignee_changed_to_none(self):
+        activity = {
+            u'event': u'Assignee changed',
+            u'text': u'New assignee: None',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u':runner: *mportela* deixou o card.')
+
+    def test_task_moved_to_peer_review(self):
+        activity = {
+            u'event': u'Task moved',
+            u'text': u"to 'In Progress.Revis\xe3o Interna'",
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u":mag_right: *mportela* enviou a tarefa para review.")
+
+    def test_(self):
+        return
+        activity = {
+            u'event': u'Assignee changed',
+            u'text': u'New assignee: None',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u"")
+
+
+if __name__ == '__main__':
+    unittest.main()
