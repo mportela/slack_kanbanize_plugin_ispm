80133b299dbdee0de464334b1105b50c09082873
Marcio <marcio.cruz@ispm.com.br>
2014-10-14 18:00:44 -0300

    novos casos

--- slack_kanbanize_formatter_plugin/ispm_formatter.py
+++ slack_kanbanize_formatter_plugin/ispm_formatter.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+import re
 
 def formatter(activity_data):
     """
@@ -15,44 +16,84 @@ def formatter(activity_data):
     """
     events_emoji_traslator = {
         u'Task archived': u':+1:',
-        u'Assignee changed': u':octocat:',
-        u'Comment added': u':speech_balloon:',
-        u'Task moved': u':rocket:',
         u'Attachments updated': u':paperclip:',
-        u'Task updated': u':pencil:',
         u'Task created': u':ticket:',
         u'External link changed': u':link:',
         u'Tags changed': u':triangular_flag_on_post:'
         }
 
-    emoji = u''
     event = activity_data.get(u'event', u'')
     user = u'*%s*' % activity_data.get(u'author', u'')
     text = activity_data.get(u'text', u'')
 
-    try:
-        emoji = u'%s ' % events_emoji_traslator[event]
+    if event == u'Assignee changed':
+        new_assignee = text.split('New assignee: ')[1]
 
-        if event == u'Assignee changed' and text == u'New assignee: None':
-            msg = u':runner: %s deixou o card.' % user
-            return msg
-
-        if event == u'Task moved' and\
-            u"to 'In Progress.Revis\xe3o Interna'" in text:
-            msg = u':mag_right: %s enviou a tarefa para review.'
+        if new_assignee == 'None':
+            msg = u':runner: %s deixou o card.'
 
             return msg % user
 
-        if event == u'Tags changed' and u'New tag: empacotado' in text:
-            msg = u':package: %s gerou pacote com a tarefa.'
+        # old card owner is intentionally not identified
+        msg = u':bust_in_silhouette: %s assumiu o card.' % user
 
-            return msg % user
+        return msg
 
-    except KeyError, e:
-        # case of event not know, just return the name with italic format
-        event = u'_%s_' % event
+    if event == u'Task moved' and\
+        u"to 'In Progress.Revis\xe3o Interna'" in text:
+        msg = u':mag_right: %s enviou a tarefa para review.'
 
-    msg = u"%sUsuário: %s Evento: %s"\
-          u": %s" % (emoji, user, event, text)
+        return msg % user
+
+    if event == u'Tags changed' and u'New tag: empacotado' in text:
+        msg = u':package: %s gerou pacote com a tarefa.'
+
+        return msg % user
+
+    if event == u'Task updated':
+        msg = u':pencil: %s atualizou a descrição do card para _"%s"_.'
+        
+        return msg % (user, text)
+
+    if event == u'Task created':
+        title = text.split('Task ')[1]
+        msg = u':o: %s criou o card _"%s"_.'
+        
+        return msg % (user, title)
+
+    if event == u'Comment added':
+        msg = u':speech_balloon: %s comentou no card: _"%s"_.'
+        
+        return msg % (user, text)
+
+    if event == u'Task moved':
+        emoji = ':heavy_check_mark:'
+
+        if text == u'The task was reordered within the same board cell.':
+            msg = u'%s %s alterou a posição do card.'
+            
+            return msg % (emoji, user)
+
+        if 'From' in text and 'to' in text:
+            from_col, to_col = re.findall("\'(.*?)\'", text, re.U)
+
+            if from_col.split('.')[0] == to_col.split('.')[0]:
+                from_col = from_col.split('.')[1]
+                to_col = to_col.split('.')[1]
+
+            msg = u"%s %s moveu o card de _%s_ para _%s_."
+
+            return msg % (emoji, user, from_col, to_col)
+
+    if event == "Task archived":
+        title = text.split('Title ')[1]
+        msg = u':toilet: %s arquivou o card _"%s"_.'
+        
+        return msg % (user, title)
+        
+
+    # default case
+    msg = u":question: Usuário: %s Evento: *%s*"\
+          u": _%s_." % (user, event, text)
 
     return msg
--- slack_kanbanize_formatter_plugin/tests.py
+++ slack_kanbanize_formatter_plugin/tests.py
@@ -5,17 +5,17 @@ from ispm_formatter import formatter
 
 
 class IspmFormatterTests(unittest.TestCase):
-    def test_default_cases(self):
+    def test_unknow_case(self):
         activity = {
-            u'event': u'Comment added',
-            u'text': u'teste',
+            u'event': u'Untreated event',
+            u'text': u'something happened in the board',
             u'author': u'mportela'
         }
 
         msg = formatter(activity)
 
         self.assertEquals(msg,
-            u':speech_balloon: Usuário: *mportela* Evento: Comment added: teste')
+            u':question: Usuário: *mportela* Evento: *Untreated event*: _something happened in the board_.')
 
     def test_assignee_changed_to_none(self):
         activity = {
@@ -53,8 +53,127 @@ class IspmFormatterTests(unittest.TestCase):
         self.assertEquals(msg,
             u":package: *mportela* gerou pacote com a tarefa.")
 
-    def test_(self):
-        return
+    def test_change_description(self):
+        activity = {
+            u'event': u'Task updated',
+            u'text': u'nova descrição',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u':pencil: *mportela* atualizou a descrição do card para _"nova descrição"_.')
+
+    def test_change_order(self):
+        activity = {
+            u'event': u'Task moved',
+            u'text': u'The task was reordered within the same board cell.',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u":heavy_check_mark: *mportela* alterou a posição do card.")
+
+    def test_self_assign(self):
+        activity = {
+            u'event': u'Assignee changed',
+            u'text': u'New assignee: mportela',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u":bust_in_silhouette: *mportela* assumiu o card.")
+
+    def test_comment(self):
+        activity = {
+            u'event': u'Comment added',
+            u'text': u'lero lero',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u':speech_balloon: *mportela* comentou no card: _"lero lero"_.')
+
+    def test_archive_card(self):
+        activity = {
+            u'event': u'Task archived',
+            u'text': u'Title Tarefa',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u':toilet: *mportela* arquivou o card _"Tarefa"_.')
+
+    def test_created_card(self):
+        activity = {
+            u'event': u'Task created',
+            u'text': u'Task nova Tarefa',
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u':o: *mportela* criou o card _"nova Tarefa"_.')
+
+    def test_change_column_same_lane(self):
+        activity = {
+            u'event': u'Task moved',
+            u'text': u"From 'Swimlane 1.Column 1' to 'Swimlane 1.Column 2'",
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u":heavy_check_mark: *mportela* moveu o card de _Column 1_ para _Column 2_.")
+
+    def test_change_column_single_name(self):
+        activity = {
+            u'event': u'Task moved',
+            u'text': u"From 'Column 1' to 'Swimlane 1.Column 2'",
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+           u":heavy_check_mark: *mportela* moveu o card de _Column 1_ para _Swimlane 1.Column 2_.")
+
+    def test_change_column_same_lane(self):
+        activity = {
+            u'event': u'Task moved',
+            u'text': u"From 'Swimlane 1.Column 1' to 'Swimlane 1.Column 2'",
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u":heavy_check_mark: *mportela* moveu o card de _Column 1_ para _Column 2_.")
+
+    def test_change_column_different_lanes(self):
+        activity = {
+            u'event': u'Task moved',
+            u'text': u"From 'Swimlane 1.Column 1' to 'Swimlane 2.Column 2'",
+            u'author': u'mportela'
+        }
+
+        msg = formatter(activity)
+
+        self.assertEquals(msg,
+            u":heavy_check_mark: *mportela* moveu o card de _Swimlane 1.Column 1_ para _Swimlane 2.Column 2_.")
+
+    def _test_(self):
         activity = {
             u'event': u'Assignee changed',
             u'text': u'New assignee: None',
